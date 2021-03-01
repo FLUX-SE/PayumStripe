c951d1ca0c67862641bdc5bf4dc9f6cb19dd6909
Francis Hilaire <Prometee@users.noreply.github.com>
2021-03-01 19:46:28 +0100

    Add Stripe Webhook test handling (event id : evt_00000000000000)

---
+++ src/Action/Api/WebhookEvent/StripeWebhookTestAction.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FluxSE\PayumStripe\Action\Api\WebhookEvent;
+
+use FluxSE\PayumStripe\Request\Api\WebhookEvent\WebhookEvent;
+use Payum\Core\Exception\RequestNotSupportedException;
+use Stripe\Event;
+
+/**
+ * This class exists to avoid 500 error when testing the Stripe Webhook
+ */
+final class StripeWebhookTestAction extends AbstractWebhookEventAction
+{
+    protected function getSupportedEventTypes(): array
+    {
+        return [
+            Event::CHECKOUT_SESSION_COMPLETED,
+            Event::PAYMENT_INTENT_CANCELED,
+            Event::SETUP_INTENT_CANCELED,
+        ];
+    }
+
+    public function execute($request)
+    {
+        RequestNotSupportedException::assertSupports($this, $request);
+        $id = $this->retrieveEventId($request);
+
+        if ('evt_00000000000000' !== $id) {
+            throw RequestNotSupportedException::createActionNotSupported($this, $request);
+        }
+
+        // Nothing else to do here the webhook will response a 200 HTTP code
+    }
+
+    private function retrieveEventId(WebhookEvent $request): ?string
+    {
+        $eventWrapper = $request->getEventWrapper();
+        /*
+         * This should never be true because the method
+         * `$this->supportTypes()` already check this
+         * @see AbstractWebhookEventAction::supportsTypes()
+         */
+        if (null === $eventWrapper) {
+            return null;
+        }
+
+        return $eventWrapper->getEvent()->id;
+    }
+}
---
+++ tests/Action/Api/WebhookEvent/StripeWebhookTestActionTest.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\FluxSE\PayumStripe\Action\Api\WebhookEvent;
+
+use FluxSE\PayumStripe\Action\Api\WebhookEvent\AbstractWebhookEventAction;
+use FluxSE\PayumStripe\Action\Api\WebhookEvent\StripeWebhookTestAction;
+use FluxSE\PayumStripe\Request\Api\WebhookEvent\WebhookEvent;
+use FluxSE\PayumStripe\Wrapper\EventWrapper;
+use Payum\Core\Action\ActionInterface;
+use Payum\Core\ApiAwareInterface;
+use Payum\Core\Exception\RequestNotSupportedException;
+use PHPUnit\Framework\TestCase;
+use Stripe\Event;
+use Tests\FluxSE\PayumStripe\Action\GatewayAwareTestTrait;
+
+final class StripeWebhookTestActionTest extends TestCase
+{
+    use GatewayAwareTestTrait;
+
+    public function testShouldImplements()
+    {
+        $action = new StripeWebhookTestAction();
+
+        $this->assertNotInstanceOf(ApiAwareInterface::class, $action);
+        $this->assertInstanceOf(ActionInterface::class, $action);
+
+        $this->assertInstanceOf(AbstractWebhookEventAction::class, $action);
+    }
+
+    public function testShouldConsumeTheWebhookEvent()
+    {
+        $model = [
+            'id' => 'evt_00000000000000',
+            'type' => Event::SETUP_INTENT_CANCELED,
+        ];
+
+        $event = Event::constructFrom($model);
+
+        $action = new StripeWebhookTestAction();
+        $eventWrapper = new EventWrapper('', $event);
+        $webhookEvent = new WebhookEvent($eventWrapper);
+
+        $action->execute($webhookEvent);
+
+        $this->assertInstanceOf(StripeWebhookTestAction::class, $action);
+    }
+
+    public function testShouldNotConsumeTheWebhookEvent()
+    {
+        $model = [
+            'id' => 'evt_00000000000001',
+            'type' => Event::SETUP_INTENT_CANCELED,
+        ];
+
+        $event = Event::constructFrom($model);
+
+        $action = new StripeWebhookTestAction();
+        $eventWrapper = new EventWrapper('', $event);
+        $webhookEvent = new WebhookEvent($eventWrapper);
+
+        $this->expectException(RequestNotSupportedException::class);
+        $action->execute($webhookEvent);
+    }
+}
